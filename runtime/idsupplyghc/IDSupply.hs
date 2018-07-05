046c03bcd8e27bfec4196c71964ffdc98d0d2758
Michael Hanus <mh@informatik.uni-kiel.de>
2018-07-05 13:33:38 +0200

    Fix compatibility with GHC 8.4

--- runtime/idsupplyghc/IDSupply.hs
+++ runtime/idsupplyghc/IDSupply.hs
@@ -1,19 +1,27 @@
 -- ---------------------------------------------------------------------------
 -- | IDSupply implementation using GHC's UniqSupply
 -- ---------------------------------------------------------------------------
+{-# LANGUAGE CPP #-}
+
 module IDSupply
   ( IDSupply, initSupply, leftSupply, rightSupply, unique
   , Unique, mkInteger, showUnique
   , getDecisionRaw, setDecisionRaw, unsetDecisionRaw
   ) where
 
-import Control.Monad (liftM)
-import Data.IORef (IORef, newIORef, readIORef, modifyIORef)
-import qualified Data.Map as Map (Map, empty, delete, findWithDefault, insert)
+import Control.Monad    (liftM)
+import Data.IORef       (IORef, newIORef, readIORef, modifyIORef)
+import Data.Maybe       (fromMaybe)
 import System.IO.Unsafe (unsafePerformIO)
-import UniqSupply
-  (UniqSupply, mkSplitUniqSupply, splitUniqSupply, uniqFromSupply)
-import Unique (Unique, getKey)
+import UniqSupply       (UniqSupply, mkSplitUniqSupply, splitUniqSupply,
+                         uniqFromSupply)
+import Unique           (Unique, getKey)
+
+#if __GLASGOW_HASKELL__ < 800
+import qualified Data.Map as Map (Map, empty, delete, findWithDefault, insert)
+#else
+import UniqDFM          (UniqDFM, emptyUDFM, delFromUDFM, lookupUDFM, addToUDFM)
+#endif
 
 -- SOURCE pragma to allow mutually recursive dependency
 import {-# SOURCE #-} ID (Decision, defaultDecision, isDefaultDecision)
@@ -47,6 +55,7 @@ rightSupply = IDSupply . snd . splitUniqSupply . uniqSupply
 unique :: IDSupply -> Unique
 unique = uniqFromSupply . uniqSupply
 
+#if __GLASGOW_HASKELL__ < 800
 -- |Internal store for 'Decision's
 store :: IORef (Map.Map Unique Decision)
 store = unsafePerformIO (newIORef Map.empty)
@@ -63,3 +72,21 @@ setDecisionRaw u c
 
 unsetDecisionRaw :: Unique -> IO ()
 unsetDecisionRaw = modifyIORef store . Map.delete
+#else
+-- |Internal store for 'Decision's
+store :: IORef (UniqDFM Decision)
+store = unsafePerformIO (newIORef emptyUDFM)
+{-# NOINLINE store #-}
+
+getDecisionRaw :: Unique -> IO Decision
+getDecisionRaw u = (fromMaybe defaultDecision . flip lookupUDFM u)
+                   `liftM` readIORef store
+
+setDecisionRaw :: Unique -> Decision -> IO ()
+setDecisionRaw u c
+  | isDefaultDecision c = modifyIORef store $ flip delFromUDFM u -- collect garbage
+  | otherwise           = modifyIORef store $ \ m -> addToUDFM m u c
+
+unsetDecisionRaw :: Unique -> IO ()
+unsetDecisionRaw = modifyIORef store . flip delFromUDFM
+#endif
