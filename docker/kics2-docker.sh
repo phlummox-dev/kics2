87a55b7695c2cedf4de445028f1a45e0a2f43f7f
Michael Hanus <mh@informatik.uni-kiel.de>
2020-10-14 18:11:27 +0200

    Docker files improved

--- docker/kics2-docker.sh
+++ docker/kics2-docker.sh
@@ -15,18 +15,12 @@ DOCKERTAG="caups/kics2"
 ENTRYPOINT=""
 HELP=no
 
-# check whether an installed tool should be invoked:
 case $1 in
   --help | -h | -\? ) HELP=yes ;;
-  -t                ) shift ; DOCKERTAG=$1 ; shift ;;
-  kics2             ) shift ;;
-  cypm              ) shift ; ENTRYPOINT="/kics2/kics2/bin/cypm" ;;
-  curry-check       ) shift ; ENTRYPOINT="/kics2/cpm/bin/curry-check" ;;
-  curry-doc         ) shift ; ENTRYPOINT="/kics2/cpm/bin/curry-doc" ;;
 esac
 
 if [ $HELP = yes ] ; then
-  echo "Usage: kics2-docker.sh [options]"
+  echo "Usage: kics2-docker.sh [-h|-?|--help] [-t TAG] [options]"
   echo ""
   echo "with options:"
   echo ""
@@ -39,6 +33,19 @@ if [ $HELP = yes ] ; then
   echo "<opts>             : invoke KiCS2 with <opts>"
   exit
 fi
+
+# check docker image tag:
+if [ $# -gt 1 -a $1 = "-t" ] ; then
+  shift ; DOCKERTAG=$1 ; shift
+fi
+
+# check whether an installed tool should be invoked:
+case $1 in
+  kics2             ) shift ;;
+  cypm              ) shift ; ENTRYPOINT="/kics2/kics2/bin/cypm" ;;
+  curry-check       ) shift ; ENTRYPOINT="/kics2/cpm/bin/curry-check" ;;
+  curry-doc         ) shift ; ENTRYPOINT="/kics2/cpm/bin/curry-doc" ;;
+esac
 
 if [ -n "$ENTRYPOINT" ] ; then
   DOCKEROPTS="$DOCKEROPTS --entrypoint=$ENTRYPOINT"
